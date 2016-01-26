159f02ae3960f9936a3c2a38c75f0ee0d18b4961
Martin Radvenis <bcmrad@live.ca>
2016-01-25 19:45:06 -0500

    microposts done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,18 @@
 class StaticPagesController < ApplicationController
-  def index
-  	render text: "Hello, this is an explicit call to controller action."
-  	return 
-  end 
 
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
   end
 
   def about
-  end 
-  
+  end
+
   def contact
   end
 end
